8c2e1d9c29f8a2c45018076c12bac71580b06098
zile028 <zile028@gmail.com>
2021-08-25 21:22:07 +0200

    navbar and header

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Izazov - 2</title>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <!-- nav -->
+    <nav>
+        <div class="container">
+            <a href="index.php">webcon</a>
+            <ul>
+                <li><a href="index.php">naslovna</a></li>
+                <li><a href="blog.php">blog</a></li>
+                <li><a href="onama.php">o nama</a></li>
+                <li><a href="radionice.php">radionice</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <!-- header -->
+    <header class="main">
+        <article class="container">
+            <div>
+                <h1>
+                    Webcon radionice –<br>
+                    kodirajte sa nama
+                </h1>
+                <p>
+                    Učite kroz konkretne, praktične primere kao i prave sajtove za klijente.<br>
+                    Pratite uživo, a posle možete da se podsetite kroz snimke koje dobijate posle radionice.
+                </p>
+                <a class="btn btn-orange" href="radionice.php">radionice<span>&RightArrow;</span></a>
+            </div>
+            <div>
+                <img src="img/header-img.png" alt="">
+            </div>
+        </article>
+    </header>
+
+
+</body>
+
+</html>
