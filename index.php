56f13a6af7ea775498ede9dc66706b0091af705e
zile028 <zile028@gmail.com>
2021-08-26 21:28:43 +0200

    uvod, onama, utisci

--- index.php
+++ index.php
@@ -6,6 +6,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Izazov - 2</title>
+    <!-- Animate style | visit: https://animate.style/ -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+
+    <!-- Font Awesome 5 | visit: https://fontawesome.com/v5.15/icons?d=gallery&p=2 -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        referrerpolicy="no-referrer" />
+
+    <!-- owl-carousel | visit: https://owlcarousel2.github.io/OwlCarousel2/-->
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
 
     <link rel="stylesheet" href="css/style.css">
 </head>
@@ -28,11 +38,11 @@
     <header class="main">
         <article class="container">
             <div>
-                <h1>
+                <h1 class="animate__animated animate__fadeInDownBig">
                     Webcon radionice –<br>
                     kodirajte sa nama
                 </h1>
-                <p>
+                <p class="animate__animated animate__fadeInUpBig">
                     Učite kroz konkretne, praktične primere kao i prave sajtove za klijente.<br>
                     Pratite uživo, a posle možete da se podsetite kroz snimke koje dobijate posle radionice.
                 </p>
@@ -44,7 +54,135 @@
         </article>
     </header>
 
+    <!-- uvod -->
+    <section class="uvod container py">
+        <h2>Prve koderske radionice uživo u regionu</h2>
+        <div class="underline">
+            <div class="item1"></div>
+            <div class="item2"></div>
+            <div class="item3"></div>
+        </div>
+        <p class="lead">Kodiramo zajedno projekte</p>
+        <article>
+            <div>
+                <i class="fas fa-laptop-code"></i>
+                <h3>PRAKTIČNI PRIMERI</h3>
+                <p>Kodiramo sa vama zajedno uživo konkretne projekte.</p>
+            </div>
 
+            <div>
+                <i class="fas fa-laptop-code"></i>
+                <h3>OBLASTI</h3>
+                <p>Teme su HTML, CSS, JS i PHP, pripadajući framework-ci i preprocesori. Spajamo tehnologije iz veb
+                    dizajna i programiranja</p>
+            </div>
+
+            <div>
+                <i class="fas fa-laptop-code"></i>
+                <h3>PRAKTIČNI PRIMERI</h3>
+                <p>Kodiramo sa vama zajedno uživo konkretne projekte.</p>
+            </div>
+
+            <div>
+                <i class="fas fa-laptop-code"></i>
+                <h3>PRAKTIČNI PRIMERI</h3>
+                <p>Kodiramo sa vama zajedno uživo konkretne projekte.</p>
+            </div>
+
+            <div>
+                <i class="fas fa-laptop-code"></i>
+                <h3>PRAKTIČNI PRIMERI</h3>
+                <p>Kodiramo sa vama zajedno uživo konkretne projekte.</p>
+            </div>
+
+            <div>
+                <i class="fas fa-laptop-code"></i>
+                <h3>PRAKTIČNI PRIMERI</h3>
+                <p>Kodiramo sa vama zajedno uživo konkretne projekte.</p>
+            </div>
+        </article>
+        <a class="btn btn-orange" href="radionice.php">Radionice</a>
+        <a class="btn btn-violet" href="onama.php">O nama</a>
+
+    </section>
+
+    <hr class="container">
+    <!-- onama -->
+    <section class="onama container py">
+        <article>
+            <h3>NAJVAŽNIJA JE PRAKSA I ISKUSTVO</h3>
+            <p>
+                Svako ko je učio da kodira, bilo samostalno ili u nekoj školi, zna da je jedno učenje a drugo praktično
+                primeniti znanje na konkretnim primerima.
+                <br><br>
+                Znanje postane upotrebljivo tek onda kad se primeni na realnim projektima.
+            </p>
+            <h3>AKO SE PITATE ZAŠTO BAŠ DA SA NAMA UČITE I VEŽBATE</h3>
+            <p>
+                Zato što se dugo za ovo spremamo. Sada znamo da možemo.
+                <br><br>
+                Sve o nama možete pogledati OVDE.
+                <br><br>
+                I najvažnije, najsvežije utiske naših polaznika koji su sada na našim online kursevima OVDE .
+            </p>
+        </article>
+
+        <article>
+            <img src="img/onama.jpg" alt="">
+        </article>
+    </section>
+
+    <!-- utisci -->
+    <section class="utisci">
+        <article class="container">
+            <div class="card">
+                <h3>Snimci radionica</h3>
+                <div>
+                    <h4>Besplatan sadrzaj</h4>
+
+                    <p>Za sve one koji zele da se oprobaju i uce sa nama, imate snimke radionica i promotivne periode
+                        placenih.</p>
+                    <a class="btn btn-orange" href="radionice.php">radionice<span>&RightArrow;</span></a>
+                    <small>* Pridruzite nam se</small>
+                </div>
+            </div>
+            <div class="slider py">
+                <div class="owl-carousel owl-theme">
+                    <div class="item">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere, temporibus velit. Harum
+                            assumenda impedit quod ut aliquid? Optio libero reiciendis similique doloribus nulla,
+                            veritatis ducimus illo, eos deserunt molestiae hic.</p>
+                        <small>Flip Jotic</small>
+                    </div>
+
+                    <div class="item">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere, temporibus velit. Harum
+                            assumenda impedit quod ut aliquid? Optio libero reiciendis similique doloribus nulla,
+                            veritatis ducimus illo, eos deserunt molestiae hic.</p>
+                        <small>Flip Jotic</small>
+                    </div>
+
+                    <div class="item">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere, temporibus velit. Harum
+                            assumenda impedit quod ut aliquid? Optio libero reiciendis similique doloribus nulla,
+                            veritatis ducimus illo, eos deserunt molestiae hic.</p>
+                        <small>Flip Jotic</small>
+                    </div>
+
+
+                </div>
+            </div>
+
+        </article>
+
+    </section>
+
+
+    <!-- owl-carousel-js - move before closed body tags -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" referrerpolicy="no-referrer">
+    </script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
 </body>
 
 </html>
